dad2a109c07ef9bfd9bd91806d99b8a6747c1bbc
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-06 00:53:58 +0300

    add home-manager inputs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
       home-manager.url = "github:nix-community/home-manager/release-23.05";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ...}@attrs: {
