0ec88d704c30657ed06ccf8c2bb5c981994586f8
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-06 18:28:21 +0300

    why wont this workkk

---
+++ modules/dev/cpp.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ...}:
+with lib;
+let cfg = config.modules.dev.cpp;
+in {
+  options.modules.dev.cpp = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    users.users.sako.packages = with pkgs; [
+      cmake
+      gcc
+    ]; 
+  };
+}
