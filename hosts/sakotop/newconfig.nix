c9353af4a1d6aa9ff10822751a45509171b270ac
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-07 00:52:42 +0300

    start making new config file and make qutebrowser

---
+++ hosts/sakotop/newconfig.nix
@@ -0,0 +1,4 @@
+{ options, config, lib, pkgs, ...}:
+{
+  #TODO(sako): add stuff
+}
---
+++ modules/desktop/web/qutebrowser.nix
@@ -0,0 +1,31 @@
+{ options, config, lib, pkgs, ...}:
+with lib;
+let cfg = config.modules.web.qutebrowser;
+in {
+  options.modules.web.qutebrowser = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    users.users.sako = {
+      packages = with pkgs; [
+        qutebrowser-qt6
+        python310Packages.pynacl
+        python310Packages.adblock
+      ];
+    };
+    
+    nixpkgs.overlays = [
+      (final: prev: { qutebrowser = prev.qutebrowser.override { enableWideVine = true; }; })
+    ];
+    xdg.configFile = {
+      "qutebrowser/config.py" = {
+        source = ../../../config/qutebrowser/config.py;
+      };
+      "qutebrowser/greasemonkey" = {
+        source = ../../../config/qutebrowser/greasemonkey;
+        recursive = true;
+      };
+    };
+  };
+}
