e503583cda6bc20dbb0b1e5805ff5817bca90240
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-15 19:12:17 +0300

    maybe add zsh plugin

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -13,7 +13,20 @@ in
     
     programs.zsh = {
       enable = true;
+      enableCompletion = true;
       promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      plugins = [
+        {
+         name = "zsh-nix-shell";
+         file = "nix-shell.plugin.zsh";
+         src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.7.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          };
+        }
+      ];
     };
 
     # for theme
