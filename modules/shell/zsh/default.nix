87e05031d774ad77a801622c2eece56751fd36a5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-15 19:14:15 +0300

    home-manager

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -15,18 +15,21 @@ in
       enable = true;
       enableCompletion = true;
       promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    };
+
+    home-manager.users.sako.programs.zsh = {
       plugins = [
         {
-         name = "zsh-nix-shell";
-         file = "nix-shell.plugin.zsh";
-         src = pkgs.fetchFromGitHub {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
             owner = "chisui";
             repo = "zsh-nix-shell";
             rev = "v0.7.0";
             sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
           };
         }
-      ];
+      ]; 
     };
 
     # for theme
