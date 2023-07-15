372af485aa0207db4ddd506b198e88b34e1c26ff
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-15 17:21:29 +0300

    kde connect fix

--- modules/desktop/apps/kdeconnect/default.nix
+++ modules/desktop/apps/kdeconnect/default.nix
@@ -13,5 +13,11 @@ in
     #  kdeconnect
     #];
     programs.kdeconnect.enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];
   };
 }
