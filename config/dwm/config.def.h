187e8d9369dddd984c32d1932c8fe06d0b3af3b9
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-20 16:47:23 +0300

    wwwwwwwwwwwwwwwwwww

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -72,7 +72,7 @@ static const char *termcmd[] = {"kitty", NULL};
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_p, spawn, {.v = roficmd}},
-    {MODKEY XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
@@ -80,7 +80,7 @@ static const Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, | ShiftMask, XK_Return, zoom, {0}},
+    {MODKEY | ShiftMask, XK_Return, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
