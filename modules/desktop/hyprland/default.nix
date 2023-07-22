873027fd404fd479d100d89fa59d17087f663b27
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-22 23:35:21 +0300

    fix

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -12,8 +12,16 @@ in
     services.xserver = {
       enable = true;
       displayManager = {
-        sddm = {
+        lightdm = {
           enable = true;
+          background = ../../../config/background.png;
+          greeters.gtk = {
+            enable = true;
+            theme = {
+              name = "vimix-dark-ruby";
+              package = pkgs.vimix-gtk-themes;
+            };
+          };
         };
       };
       libinput = {
