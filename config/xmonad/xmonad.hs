1885f46b790a219b71f0122e6a92e27869afbfb9
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-23 17:04:05 +0300

    that didnt do it right

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -131,7 +131,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_q     ), io exitSuccess)
 
     -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart; killall xmobar")
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; killall xmobar; xmonad --restart")
 
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), xmessage help)
