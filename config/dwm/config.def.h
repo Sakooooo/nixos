c2becbeccfb2015e98e0cd775db2ed6cd0284c2b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-24 16:09:04 +0300

    everything everything black

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -13,7 +13,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono NF:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+//static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#cccccc";
 static const char col_gray4[]       = "#eeeeee";
