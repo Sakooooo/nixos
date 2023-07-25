cce4c62f71be1ffd91e608706f3ca71d061cf647
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-25 07:41:08 +0300

    fix audio keys?

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -135,9 +135,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     --
     -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
     -- Audio
-    , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +5%")
-    , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume0 -5%")
-    , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
+    , (xF86XK_AudioRaiseVolume, spawn "pactl set-sink-volume 0 +5%")
+    , (xF86XK_AudioLowerVolume, spawn "pactl set-sink-volume0 -5%")
+    , (xF86XK_AudioMute, spawn "pactl set-sink-mute 0 toggle")
 
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io exitSuccess)
