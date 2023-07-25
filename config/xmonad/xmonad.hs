bc99fecc8ab6ec4f3b86e2fb5d51e8b66afefa20
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-26 00:59:21 +0300

    Revert "missing import"
    This reverts commit 371bb170f3259408b97f64d280875875561b91de.

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -28,7 +28,6 @@ import Graphics.X11.ExtraTypes.XF86 -- for keys
 import XMonad.Util.Loggers -- smth
 import XMonad.Layout.MultiToggle -- fulllscreen
 import XMonad.Layout.MultiToggle.Instances
-import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.NoBorders (noBorders)
 
 import qualified XMonad.StackSet as W
