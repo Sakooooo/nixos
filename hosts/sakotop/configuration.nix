8526d86feb51f5bd98bfecf7a7ef1c372a605d95
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-28 19:01:19 +0300

    :p

--- hosts/sakotop/configuration.nix
+++ hosts/sakotop/configuration.nix
@@ -14,10 +14,10 @@
   modules = {
     desktop = {
       kitty.enable = true;
-      xmonad.enable = true;
+      xmonad.enable = false;
       dwm = {
-        enable = false;
-        dwmblocks.enable = false;
+        enable = true;
+        dwmblocks.enable = true;
       };
       dunst.enable = true;
       browsers = {
