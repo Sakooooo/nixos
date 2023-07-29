8421c1c8a05f646153c790ef3700409c6902c722
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-29 18:21:38 +0300

    we

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -33,6 +33,7 @@ static const char *const autostart[] = {
   "blueman-applet", NULL,
   "keepassxc", NULL,
   "flameshot", NULL,
+  "discord", NULL,
 	NULL /* terminate */
 };
 
@@ -48,7 +49,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "discord",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
