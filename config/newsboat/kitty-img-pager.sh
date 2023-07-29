b15108ceed0c20aec99649217aff9835716cf7f1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-29 16:24:05 +0300

    lmao

--- config/newsboat/kitty-img-pager.sh
+++ config/newsboat/kitty-img-pager.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Shows images, scaled to terminal dimensions, via kitty's icat.
 # Designed for image-feeds like xkcd.com.
 # by Timm Heuss (https://github.com/heussd/) - CC-BY 4.0
@@ -8,12 +8,11 @@
 #    macro i set pager "$PATHTOTHIS/kitty-img-pager"; open; set pager internal
 # 2) Now you use this pager in articleview using the hotkey <comma>+<i>
 
-set -o errexit
-set -o pipefail
-set -o nounset
+#set -o errexit
+#set -o pipefail
+#set -o nounset
 
-
-image=$(grep -E -o "https?://[a-zA-Z0-9./?=_%:-]*" < "${#}" | tail -1)
+image=$(grep -E -o "https?://[a-zA-Z0-9./?=_%:-]*" < "${1}" | tail -1)
 
 # Detect terminal dimensions
 dims="$(tput cols)x$(tput lines)@0x0"
