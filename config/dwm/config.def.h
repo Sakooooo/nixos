969cff6da9bd875e04f2c4b88560278cbbb55f0f
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-29 17:47:28 +0300

    change workspace names

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -38,7 +38,7 @@ static const char *const autostart[] = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "web", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "web", "dev", "irc", "media", "game", "misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
