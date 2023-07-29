6acecc697e5a6bfdda1ed96bb775c15d9ceeee4d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-29 16:08:02 +0300

    newsboat configuration

--- modules/shell/newsboat/default.nix
+++ modules/shell/newsboat/default.nix
@@ -12,5 +12,13 @@ in
     users.users.sako.packages = with pkgs; [
       newsboat
     ];
+    home-manager.users.sako = { pkgs, ...}: {
+      xdg.configFile = {
+        newsboat = {
+          source = ../../../config/newsboat;
+          recursive = true;
+        };
+      };
+    };
   };
 }
