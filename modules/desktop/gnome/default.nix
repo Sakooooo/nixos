bce2fbf0c82141b4243d1d4b55bfda6d612efcf8
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-30 16:45:00 +0300

    add gnome

---
+++ modules/desktop/gnome/default.nix
@@ -0,0 +1,36 @@
+{ outputs, options, config, lib, pkgs, ...}:
+with lib;
+let
+  cfg = config.modules.desktop.gnome;
+in
+{
+  options.modules.desktop.gnome = {
+    enable = mkEnableOption false;
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver = {
+      enable = true;
+      displayManager = {
+        gdm = {
+          enable = true;
+          # not taking any chances
+          wayland = false;
+        };
+      };
+      desktopManager = {
+        gnome = {
+          enable = true;
+        };
+      };
+    };
+    # gnome extensions
+    environment.systemPackages = with pkgs; [ 
+      gnomeExtensions.appindicator 
+    ];
+    # gnome udev settings
+    services.udev.packages = with pkgs; [
+      gnome.gnome-settings-daemon 
+    ];
+  };
+}
