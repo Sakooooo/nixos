fff65118720f8a00290333496e4959eb9daa63af
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-30 20:20:16 +0300

    horay

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -89,7 +89,7 @@ static const char *termcmd[]  = { "kitty", NULL };
 // audio
 static const char *upvol[]      = { "/usr/bin/pactl",   "set-sink-volume", "0",      "+5%",      NULL };
 static const char *downvol[]    = { "/usr/bin/pactl",   "set-sink-volume", "0",      "-5%",      NULL };
-static const char *mutevol[]    = { "/usr/bin/pactl",   "set-sink-mute",   "0",      "toggle", 
+static const char *mutevol[]    = { "/usr/bin/pactl",   "set-sink-mute",   "0",      "toggle",   NULL }; 
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
