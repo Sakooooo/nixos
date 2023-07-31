47e248986234849509a99e50f6610440485c8141
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-07-31 22:53:22 +0300

    spelt wrong

--- hosts/sakotop/configuration.nix
+++ hosts/sakotop/configuration.nix
@@ -29,7 +29,7 @@
         keepassxc.enable = true;
         nicotineplus.enable = true;
         transmission.enable = true;
-        kcc.enable = true;
+        kindle-comic-converter.enable = true;
       };
       chat = {
         discord.enable = true;
