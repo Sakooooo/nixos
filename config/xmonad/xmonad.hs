bca7b5db349ec8a8661e63b08112acdba4171a37
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-02 15:18:31 +0300

    ?

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -299,7 +299,7 @@ myStartupHook = do
   spawnOnce "nm-applet"
   spawnOnce "blueman-applet"
   spawnOnce "flameshot"
-  spawn "trayer --edge bottom --align right --iconspacing 5 --height 20 --widthtype request --alpha 0 --transparent true --tint 'black'"
+  spawn "trayer --edge bottom --align right --iconspacing 5 --height 30 --widthtype request --alpha 0 --transparent true --tint 'black'"
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
