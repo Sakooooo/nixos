38e6b79ed8617e46bdcc30d388b7f2e08c9255a2
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-03 01:47:05 +0300

    ill do this later

---
+++ modules/desktop/qtile/default.nix
@@ -0,0 +1,92 @@
+{ outputs, options, config, lib, pkgs, ...}:
+with lib;
+let
+  cfg = config.modules.desktop.qtile;
+in
+{
+  options.modules.desktop.qtile = {
+    enable = mkEnableOption false;
+  };
+
+  #TODO Do this
+
+  config = mkIf cfg.enable {
+    services.xserver = {
+      enable = true;
+      windowManager.qtile = {
+        enable = true;
+      };
+      displayManager = {
+       defaultSession = "none+qtile"; 
+       lightdm = {
+        enable = true;
+        background = ../../../config/background.png;
+        greeters.gtk = {
+          enable = true;
+          theme = {
+            name = "vimix-dark-ruby";
+            package = pkgs.vimix-gtk-themes;
+          };
+        };
+
+       };
+      };
+      libinput = {
+        enable = true;
+
+        # no mouse accel
+        mouse = {
+          accelProfile = "flat";
+        };
+
+        # no touchpad accel
+        touchpad = {
+          accelProfile = "flat";
+        };
+      };
+    };  
+    users.users.sako.packages = with pkgs; [
+      rofi
+      # network
+      networkmanagerapplet
+      # brightness
+      brightnessctl
+      # gee tee k
+      vimix-gtk-themes
+      vimix-icon-theme
+      lxappearance
+      catppuccin-cursors.mochaDark
+      # screen shot (s)
+      flameshot
+    ];
+
+    home-manager.users.sako = { pkgs , ...}: {
+      home.pointerCursor = {
+        name = "Catppuccin-Mocha-Dark"; 
+        size = 16;
+        x11 = {
+          enable = true;
+        };
+        gtk.enable = true;
+        package = pkgs.catppuccin-cursors.mochaDark;
+      };
+      gtk = {
+        theme.name = "vimix-dark-ruby";
+        iconTheme.name = "Vimix Ruby Dark";
+      };
+      home.file = {
+        "background.png" = {
+          enable = true;
+          source = ../../../config/background.png;
+        };
+      };
+      xdg.configFile = {
+        awesome = {
+          enable = true;
+          source = ../../../config/awesome;
+          recursive = true;
+        };
+     }; 
+    };
+  };
+}
