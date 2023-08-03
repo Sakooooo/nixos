62b567eb4df686002f630a3f97862dc15f03ee65
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-03 12:47:28 +0300

    Revert "add awesomewm theme"
    This reverts commit d4c79a9aa4a58acf4bff105ec024e26d0fb68703.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "/themes/dark/dark.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
