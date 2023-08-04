eb8c050d71faf653a29b3ffee45beef48a4e36b4
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-05 02:12:15 +0300

    forgot to add this toconfig.def.h

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -15,6 +15,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
+
 static const char *fonts[]          = { "JetBrainsMono NF:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 //static const char col_gray1[]       = "#222222";
