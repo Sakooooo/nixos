cdcb5fed3dbe2d89d6688b9b5388cb31c4913914
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-06 01:38:42 +0300

    exclude dwm from picom and change font size

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NF:size=12" };
+static const char *fonts[]          = { "JetBrainsMono NF:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 //static const char col_gray1[]       = "#222222";
 static const char col_gray1[]       = "#000000";
@@ -46,7 +46,7 @@ static const char *const autostart[] = {
 
 /* tagging */
 // static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { " 󰈹 ", "  ", " 󰭹 ", " 󰕧 ", "  " };
+static const char *tags[] = { " 󰈹  ", "   ", " 󰭹  ", " 󰕧  ", "   " };
 
 static const Rule rules[] = {
 	/* xprop(1):
