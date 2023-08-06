db04c010331dc3035a2a94e886b1519bccceeb94
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-07 00:50:09 +0300

    fix icons missing

--- config/awesome/themes/default/theme.lua
+++ config/awesome/themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources                                = require("beautiful.xresources"
 local dpi                                       = xresources.apply_dpi
 
 local gfs                                       = require("gears.filesystem")
-local themes_path                               = gfs.get_themes_dir()
+local themes_path                               = gfs.get_configuration_dir()
 
 local theme                                     = {}
 
