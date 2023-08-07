e3cf0ade4625c5f43655ea75944d86da6a157c81
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-08 00:01:29 +0300

    change telescope keybinds

--- config/nvim/lua/core/plugin_config/telescope.lua
+++ config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
+vim.keymap.set('n', '<Space><Space>', builtin.find_files, {})
+-- vim.keymap.set('n', '<Space><p>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
