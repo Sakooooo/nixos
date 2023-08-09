5bab9d869d01b28f3d46e757f08315e3619aefe7
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-09 21:37:37 +0300

    bottom bar breaks tray apps

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -14,7 +14,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono NF:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 //static const char col_gray1[]       = "#222222";
