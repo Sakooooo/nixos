052f0fec830dee70cec7ee5f5c5ad2db34079c24
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 18:16:10 +0300

    s

--- config/nvim/lua/core/plugin_config/colorscheme.lua
+++ config/nvim/lua/core/plugin_config/colorscheme.lua
@@ -1,24 +1,24 @@
 require("catppuccin").setup({
-  flavour = "mocha",   -- latte, frappe, macchiato, mocha
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
   background = {
-                       -- :h background
+    -- :h background
     light = "latte",
     dark = "mocha",
   },
-  transparent_background = false,   -- disables setting the background color.
-  show_end_of_buffer = false,       -- shows the '~' characters after the end of buffers
-  term_colors = false,              -- sets terminal colors (e.g. `g:terminal_color_0`)
+  transparent_background = false, -- disables setting the background color.
+  show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+  term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
   dim_inactive = {
-    enabled = false,                -- dims the background color of inactive window
+    enabled = false,              -- dims the background color of inactive window
     shade = "dark",
-    percentage = 0.15,              -- percentage of the shade to apply to the inactive window
+    percentage = 0.15,            -- percentage of the shade to apply to the inactive window
   },
-  no_italic = false,                -- Force no italic
-  no_bold = false,                  -- Force no bold
-  no_underline = false,             -- Force no underline
+  no_italic = false,              -- Force no italic
+  no_bold = false,                -- Force no bold
+  no_underline = false,           -- Force no underline
   styles = {
-                                    -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" },        -- Change the style of comments
+    -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
     conditionals = { "italic" },
     loops = {},
     functions = {},
@@ -32,11 +32,6 @@ require("catppuccin").setup({
     operators = {},
   },
   color_overrides = {
-    mocha = {
-      base = "#000000",
-      mantle = "#000000",
-      crust = "#000000",
-    },
   },
   custom_highlights = {},
   integrations = {
