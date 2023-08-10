61a0083ebc87c5f0dc307a3a47a95e2d03a0f3a0
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 22:03:36 +0300

    aa

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -100,17 +100,34 @@ local plugins = {
             '',
             '',
           }, --your header
-          shortcut = {
-            -- action can be a function type
-            { desc = string, group = 'highlight group', key = 'shortcut key', action = 'action when you press key' },
+          config = {
+            week_header = {
+              enable = true,
+            },
+            shortcut = {
+              { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+              {
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'f',
+              },
+              {
+                desc = ' Apps',
+                group = 'DiagnosticHint',
+                action = 'Telescope app',
+                key = 'a',
+              },
+              {
+                desc = ' dotfiles',
+                group = 'Number',
+                action = 'Telescope dotfiles',
+                key = 'd',
+              },
+            },
           },
-          packages = { enable = true }, -- show how many plugins neovim loaded
-          -- limit how many projects list, action when you press key or enter it will run this action.
-          -- action can be a functino type, e.g.
-          -- action = func(path) vim.cmd('Telescope find_files cwd=' .. path) end
-          project = { enable = true, limit = 8, icon = 'your icon', label = '', action = 'Telescope find_files cwd=' },
-          mru = { limit = 10, icon = 'your icon', label = '', },
-          footer = {}, -- footer
         }
       }
     end,
