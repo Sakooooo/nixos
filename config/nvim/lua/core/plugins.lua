e4fcbab2cf6549e241123b8e5e73e59744da35e5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 19:20:43 +0300

    we

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -73,11 +73,6 @@ local plugins = {
   {
     'glepnir/dashboard-nvim',
     event = 'VimEnter',
-    config = function()
-      require('dashboard').setup {
-        -- config
-      }
-    end,
     dependencies = { { 'nvim-tree/nvim-web-devicons' } }
   }
 }
