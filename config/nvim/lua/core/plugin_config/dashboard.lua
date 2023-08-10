9888d230e5c028ee11c15dbd75ab05a793666e75
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 19:19:43 +0300

    mf doom hes like db cooper

--- config/nvim/lua/core/plugin_config/dashboard.lua
+++ config/nvim/lua/core/plugin_config/dashboard.lua
@@ -1,5 +1,5 @@
 require('dashboard').setup {
-  theme = 'hyper',    --  theme is doom and hyper default is hyper
+  theme = 'doom',     --  theme is doom and hyper default is hyper
   disable_move,       --  default is false disable move keymap for hyper
   shortcut_type,      --  shorcut type 'letter' or 'number'
   change_to_vcs_root, -- default is false,for open file in hyper mru. it will change to the root of vcs
