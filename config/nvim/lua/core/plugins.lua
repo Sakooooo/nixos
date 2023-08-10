3e3b6ad0f3e758993c927f4a8494cae495c9e07c
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-10 19:29:57 +0300

    mf doom he like db cooper

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -74,21 +74,15 @@ local plugins = {
     'glepnir/dashboard-nvim',
     config = function()
       require('dashboard').setup {
-        theme = 'doom',     --  theme is doom and hyper default is hyper
-        disable_move,       --  default is false disable move keymap for hyper
-        shortcut_type,      --  shorcut type 'letter' or 'number'
-        change_to_vcs_root, -- default is false,for open file in hyper mru. it will change to the root of vcs
-        config = {},        --  config used for theme
-        hide = {
-          statusline,       -- hide statusline default is true
-          tabline,          -- hide the tabline
-          winbar,           -- hide winbar
-        },
-        preview = {
-          command,     -- preview command
-          file_path,   -- preview file path
-          file_height, -- preview file height
-          file_width,  -- preview file width
+        theme = 'doom', --  theme is doom and hyper default is hyper
+        {
+          icon = '',
+          icon_hl = 'group',
+          desc = 'description',
+          desc_hl = 'group',
+          key = 'shortcut key in dashboard buffer not keymap !!',
+          key_hl = 'group',
+          action = '',
         },
       }
     end,
