ffe373e174182a45e4e1aa267e531cd6f39af359
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-11 13:04:07 +0300

    lmao forgot i use git

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -72,7 +72,62 @@ local plugins = {
   -- startup
   {
     'glepnir/dashboard-nvim',
-    config = nil, 
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper', --  theme is doom and hyper default is hyper
+        config = {
+          -- todo https://github.com/nvimdev/dashboard-nvim
+          header = {
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '  █████████    █████████   █████   ████    ███████   ',
+            ' ███░░░░░███  ███░░░░░███ ░░███   ███░   ███░░░░░███ ',
+            '░███    ░░░  ░███    ░███  ░███  ███    ███     ░░███',
+            '░░█████████  ░███████████  ░███████    ░███      ░███',
+            ' ░░░░░░░░███ ░███░░░░░███  ░███░░███   ░███      ░███',
+            ' ███    ░███ ░███    ░███  ░███ ░░███  ░░███     ███',
+            '░░█████████  █████   █████ █████ ░░████ ░░░███████░',
+            '░░░░░░░░░  ░░░░░   ░░░░░ ░░░░░   ░░░░    ░░░░░░░    ',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+          }, --your header
+          config = {
+            shortcut = {
+              { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+              {
+                icon = ' ',
+                icon_hl = '@variable',
+                desc = 'Files',
+                group = 'Label',
+                action = 'Telescope find_files',
+                key = 'f',
+              },
+              {
+                desc = ' Apps',
+                group = 'DiagnosticHint',
+                action = 'Telescope app',
+                key = 'a',
+              },
+              {
+                desc = ' dotfiles',
+                group = 'Number',
+                action = 'Telescope dotfiles',
+                key = 'd',
+              },
+            },
+          },
+        }
+      }
+    end,
     event = 'VimEnter',
     dependencies = { { 'nvim-tree/nvim-web-devicons' } }
   },
