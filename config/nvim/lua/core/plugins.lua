970abc1500087db1dae65e992373fab6b785c9ad
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-11 14:20:31 +0300

    mf doom hes like, db cooper

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -88,31 +88,25 @@ local plugins = {
             '░░█████████  █████   █████ █████ ░░████ ░░░███████░',
             '░░░░░░░░░  ░░░░░   ░░░░░ ░░░░░   ░░░░    ░░░░░░░    ',
           }, --your header
-          config = {
-            shortcut = {
-              { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-              {
-                icon = ' ',
-                icon_hl = '@variable',
-                desc = 'Files',
-                group = 'Label',
-                action = 'Telescope find_files',
-                key = 'f',
-              },
-              {
-                desc = ' Apps',
-                group = 'DiagnosticHint',
-                action = 'Telescope app',
-                key = 'a',
-              },
-              {
-                desc = ' dotfiles',
-                group = 'Number',
-                action = 'Telescope dotfiles',
-                key = 'd',
-              },
+          center = {
+            {
+              icon = ' ',
+              icon_hl = 'Title',
+              desc = 'Find File           ',
+              desc_hl = 'String',
+              key = 'b',
+              keymap = 'SPC f f',
+              key_hl = 'Number',
+              action = 'lua print(2)'
             },
-          },
+            {
+              icon = ' ',
+              desc = 'Find Dotfiles',
+              key = 'f',
+              keymap = 'SPC f d',
+              action = 'lua print(3)'
+            },
+          }
         }
       }
     end,
