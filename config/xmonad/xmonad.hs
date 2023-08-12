4308729214a09c905a351fc1d03014d3e5986c5d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-12 17:47:00 +0300

    indents matter lmao

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -198,7 +198,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
- myLayout = tiled ||| Mirror tiled ||| noBorders Full
+myLayout = tiled ||| Mirror tiled ||| noBorders Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
