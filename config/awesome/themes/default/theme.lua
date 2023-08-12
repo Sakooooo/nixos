5b6dd3b59a0da0e5890ea72709ae09f492cafa92
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-12 17:15:37 +0300

    asdfasdfasdf

--- config/awesome/themes/default/theme.lua
+++ config/awesome/themes/default/theme.lua
@@ -7,7 +7,7 @@ local xresources                                = require("beautiful.xresources"
 local dpi                                       = xresources.apply_dpi
 
 local gfs                                       = require("gears.filesystem")
-local themes_path                               = gfs.get_configuration_dir() .. "/themes"
+local themes_path                               = gfs.get_configuration_dir() .. "themes/"
 
 local theme                                     = {}
 
