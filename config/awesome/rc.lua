ef3acad0f13de64cc8930219d214e662b6e24186
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-13 17:50:30 +0300

    let me try this

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -586,4 +586,19 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 beautiful.useless_gap = 10
 
 -- autostart
-awful.util.spawn_with_shell("~/.config/awesome/autostart.sh")
+--awful.util.spawn_with_shell("~/.config/awesome/autostart.sh")
+
+local run_on_start_up = {
+  'keepassxc',
+  'flameshot',
+}
+
+for _, app in ipairs(run_on_start_up) do
+  local findme = app
+  local firstspace = app:find(" ")
+  if firstspace then
+    findme = app:sub(0, firstspace - 1)
+  end
+  -- pipe commands to bash to allow command to be shell agnostic
+  awful.spawn.with_shell(string.format("echo 'pgrep -u $USER -x %s > /dev/null || (%s)' | bash -", findme, app), false)
+end
