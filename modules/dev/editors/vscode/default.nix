5042027490001a5941a1aaff58afe87f40784ac6
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-14 20:43:36 +0300

    add sha256

--- modules/dev/editors/vscode/default.nix
+++ modules/dev/editors/vscode/default.nix
@@ -19,20 +19,22 @@ in {
   config = mkIf cfg.enable {
     users.users.sako.packages = with pkgs; [
       (vscode-with-extensions.override {
-        vscodeExtensions = with vscode-extensions; [
-          mkhl.direnv
-          vscodevim.vim
-          ms-python.vscode-pylance
-          ms-vscode.cmake-tools
-          ms-dotnettools.csharp
-        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "better-comments";
-            publisher = "aaron-bond";
-            version = "3.0.2";
-            sha256 = "";
-          }
-        ];
+        vscodeExtensions = with vscode-extensions;
+          [
+            mkhl.direnv
+            vscodevim.vim
+            ms-python.vscode-pylance
+            ms-vscode.cmake-tools
+            ms-dotnettools.csharp
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "better-comments";
+              publisher = "aaron-bond";
+              version = "3.0.2";
+              sha256 = "hQmA8PWjf2Nd60v5EAuqqD8LIEu7slrNs8luc3ePgZc=";
+            }
+          ];
       })
     ];
   };
