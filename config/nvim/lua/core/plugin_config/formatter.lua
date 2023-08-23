05b0210dae2c805ccde6bdde8203ce06a43cf57c
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-23 16:52:32 +0000

    add formatter

---
+++ config/nvim/lua/core/plugin_config/formatter.lua
@@ -0,0 +1,42 @@
+-- Utilities for creating configurations
+local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+require("formatter").setup {
+  -- Enable or disable logging
+  logging = true,
+  log_level = vim.log.levels.WARN,
+  filetype = {
+    lua = {
+      require("formatter.filetypes.lua").stylua,
+      function()
+        return {
+          exe = "stylua",
+          args = {
+            "--search-parent-directories",
+            "--stdin-filepath",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--",
+            "-",
+          },
+          stdin = true,
+        }
+      end
+    },
+  nix = {
+      require("formatter.filetypes.nix").alejandra,
+      function()
+        return {
+          exe = "alejandra",
+          stdin = true,
+          args = {
+            "--quiet"
+          },
+        }
+      end
+    },
+    ["*"] = {
+      require("formatter.filetypes.any").remove_trailing_whitespace
+    }
+  }
+}
