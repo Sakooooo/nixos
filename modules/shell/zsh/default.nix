7550db239010aef401040553fa1e3f088313f2fc
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-28 09:45:17 +0000

    bump zsh plugin

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -38,7 +38,7 @@ in
         src = pkgs.fetchFromGitHub {
           owner = "chisui";
           repo = "zsh-nix-shell";
-          rev = "v0.5.0";
+          rev = "v0.7.0";
           sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
         };
         }
