595b16bcdd8e3381a73f8422dcc0da76f7c6f146
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-08-29 13:31:42 +0400

    make default floatterm bigger

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -49,7 +49,7 @@ vim.keymap.set("n", "nsh", ":split<CR>", opts)
 
 -- terminal
 --vim.keymap.set("n", "<leader>t", ":split<CR> <BAR> :terminal<CR>")
-vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR>")
+vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR> --height=25 --width=100")
 vim.keymap.set("n", "<leader>g", ":FloatermNew --height=29 --width=120 lazygit<CR>")
 
 -- exit terminal with esc
