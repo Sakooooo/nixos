52c9b1f01a650298f3236418e46465cea3c6a7e5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-04 14:36:14 +0000

    tacobell

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,22 +1,28 @@
+;; UI/UX
 (setq inhibit-startup-message t)
 
-;; go away bars, you look ugly
+;; make it look like neovim a little
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(tooltip-mode -1)
+(tooltip-mode -1
 (set-fringe-mode 10)
 
 (menu-bar-mode -1)
 
-;; bell
+;; Bell
 (setq visible-bell t)
 
-;; font
-(set-face-attribute `default nil :font "JetBrains Mono" :height 150)
+;; Font
+(set-face-attribute `default nil :font "JetBrains Mono" :height 175)
 
-(load-theme 'tango-dark)
+(load-theme 'wombat)
 
-;; packages
+;; Keymaps
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Package related stuff
 (require `package)
 
 (setq package-archives `(("mepla" . "https://melpa.org/packages/")
@@ -35,3 +41,22 @@
 (setq use-package-always-ensure t)
 
 (use-package command-log-mode)
+
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
