8d5e5a273fc94f9a175edb56841a1a4454c45d1b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-04 13:19:21 +0000

    what

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,5 +1,6 @@
 (setq inhibit-startup-message t)
 
+;; go away bars, you look ugly
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -32,3 +33,5 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+(use-package command-log-buffer)
