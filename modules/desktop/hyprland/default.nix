33c951625f2390ffe2240c6b773d18c5af615ec1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-07 08:36:24 +0400

    fix swaylock

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -112,5 +112,13 @@ in
         };
      }; 
     };
+
+    # swaylock doesnt work without this
+    security.pam.services.swaylock.text = ''
+      # PAM configuration file for the swaylock screen locker. By default, it includes
+      # the 'login' configuration file (see /etc/pam.d/login)
+      auth include login
+    '';
+
   };
 }
