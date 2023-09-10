034c018e3e76a567f246f156c937180418213b8a
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-10 21:03:51 +0400

    i love the c programming language

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -18,6 +18,13 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+  "feh", "--bg-scale", "/home/sako/background.png", NULL,
+  "xsetroot", "-cursor_name", "left_ptr", NULL,
+	"keepassxc", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
