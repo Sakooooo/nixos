c0feb057b53e3a33b154b1f505c1675a04918abb
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-11 11:37:10 +0400

    add zoom

--- modules/desktop/chat/default.nix
+++ modules/desktop/chat/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./teams
     ./whatsapp
+    ./zoom
   ];
 }
---
+++ modules/desktop/chat/zoom/default.nix
@@ -0,0 +1,16 @@
+{ outputs, options, config, lib, pkgs, ...}:
+with lib;
+let
+  cfg = config.modules.desktop.chat.zoom;
+in
+{
+  options.modules.desktop.chat.zoom = {
+    enable = mkEnableOption false;
+  };
+
+  config = mkIf cfg.enable {
+    users.users.sako.package = with pkgs; [
+      zoom-us
+    ];
+  };
+}
