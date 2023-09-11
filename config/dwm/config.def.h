cff3ff8598ad3166e14c6a44072c9c0882481d71
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-11 11:06:19 +0400

    dwm colorscheme

--- config/dwm/config.def.h
+++ config/dwm/config.def.h
@@ -12,15 +12,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black1[]       = "#000000";
+static const char col_white1[]       = "#d0d0d0";
+static const char col_red[]       = "#ff0000";
+static const char col_red2[]        = "#830202";
+static const char col_red3[]        = "#aa1212";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white1, col_gray1, col_red2},
+	[SchemeSel]  = { col_white1, col_red,  col_red3},
 };
 
 static const char *const autostart[] = {
