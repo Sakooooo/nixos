ed5520170906b30b50a18ff1971675b7773a6ee5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-11 07:44:57 +0400

    fix systray

--- config/dwm/dwm.c
+++ config/dwm/dwm.c
@@ -629,6 +629,7 @@ clientmessage(XEvent *e)
 			updatesystrayicongeom(c, wa.width, wa.height);
 			XAddToSaveSet(dpy, c->win);
 			XSelectInput(dpy, c->win, StructureNotifyMask | PropertyChangeMask | ResizeRedirectMask);
+      XClassHint ch = {"dwmsystray", "dwmsystray"};
 			XReparentWindow(dpy, c->win, systray->win, 0, 0);
 			/* use parents background color */
 			swa.background_pixel  = scheme[SchemeNorm][ColBg].pixel;
