f2b47a50a5cafc1ffaa0ea32c28b9b75bf03dbf4
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-12 12:39:27 +0400

    ::P

--- config/dwmbar/bar.sh
+++ config/dwmbar/bar.sh
@@ -6,21 +6,21 @@ battery() {
   get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
   get_status="$(cat /sys/class/power_supply/BAT1/status)"
   case "$get_status" in
-  Charging) printf "^c$blue^ 󰂄 $get_capacity" ;;
-  Discharging) printf "^b$blue 󰁹 $get_capacity" ;;
+  Charging) printf "󰂄 $get_capacity" ;;
+  Discharging) printf "󰁹 $get_capacity" ;;
   esac
 }
 
 audio() {
   get_vol=$(pamixer --get-volume-human)
   if [ $get_vol = 'muted' ]; then
-    printf "^b$blue^ 󰕾 Muted"
+    printf "󰕾 Muted"
   else
-    printf "^b$blue^ 󰕾 $get_vol"
+    printf "󰕾 $get_vol"
   fi
 }
 
 while true; do
-  xsetroot -name "$(battery)"
+  xsetroot -name " $(audio) $(battery) "
   sleep 1
 done
