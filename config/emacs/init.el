125fd8daa69f6dd4698f2b0e5ef5ec31a71ce14c
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-15 15:06:09 +0000

    wa

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -21,7 +21,7 @@
 (global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(set-face-attribute `default nil :font "JetBrains Mono" :height 125)
+(set-face-attribute `default nil :font "JetBrains Mono" :height 150)
 
 (require `package)
 
@@ -461,3 +461,36 @@
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle)))
   (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'sakomacs/org-babel-tangle-config))))
+
+(use-package term
+:config
+(setq explicit-shell-file-name "zsh")
+
+(defun sakomacs/configure-eshell ()
+  ;; Save command history when commands are entered
+  (add-hook 'eshell-pre-command-hook 'eshell-save-some-history)
+
+  ;; Truncate buffer for performance
+  (add-to-list 'eshell-output-filter-functions 'eshell-truncate-buffer)
+
+  ;; Bind some useful keys for evil-mode
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "C-r") 'counsel-esh-history)
+  (evil-define-key '(normal insert visual) eshell-mode-map (kbd "<home>") 'eshell-bol)
+  (evil-normalize-keymaps)
+
+  (setq eshell-history-size         10000
+        eshell-buffer-maximum-lines 10000
+        eshell-hist-ignoredups t
+        eshell-scroll-to-bottom-on-input t))
+
+(use-package eshell-git-prompt)
+
+(use-package eshell
+  :hook (eshell-first-time-mode . sakomacs/configure-eshell)
+  :config
+
+  (with-eval-after-load 'esh-opt
+    (setq eshell-destroy-buffer-when-process-dies t)
+    (setq eshell-visual-commands '("htop" "zsh" "vim")))
+
+  (eshell-git-prompt-use-theme 'powerline))
