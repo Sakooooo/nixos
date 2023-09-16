a7ffc641eba87da0ffb4135c2086e41ce2b7d26b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-16 14:02:37 +0400

    Revert "attempt to use wine stable to see if grapejuice will work"
    This reverts commit 0fa11770989dae347c39cf09caa1e5e2c3522b35.

--- modules/desktop/game/wine/default.nix
+++ modules/desktop/game/wine/default.nix
@@ -1,14 +1,9 @@
-{
-  outputs,
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-with lib; let
+{ outputs, options, config, lib, pkgs, ...}:
+with lib;
+let
   cfg = config.modules.desktop.game.wine;
-in {
+in
+{
   options.modules.desktop.game.wine = {
     enable = mkEnableOption false;
   };
@@ -16,7 +11,13 @@ in {
   config = mkIf cfg.enable {
     users.users.sako.packages = with pkgs; [
       winetricks
-      wineWowPackages.stable
+      wineWowPackages.staging
     ];
+    nixpkgs.config = {
+      wine = {
+        release = "unstable";
+        build = "wineWow";
+      };
+    };
   };
 }
