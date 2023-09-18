9dc81675312c152699dba68750f719be41b41ec1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-09-18 13:28:56 +0400

    finally fix sakopc config

--- hosts/sakopc/configuration.nix
+++ hosts/sakopc/configuration.nix
@@ -4,6 +4,7 @@
   lib,
   inputs,
   outputs,
+  ...
 }: {
   imports = [
     # Hardware Configuration
@@ -16,67 +17,65 @@
 
   modules = {
     desktop = {
-      desktop = {
-        dwm.enable = true;
-        kitty.enable = true;
-        picom.enable = true;
-        apps = {
-          keepassxc.enable = true;
-          obs.enable = true;
-        };
-        browsers = {
-          firefox.enable = true;
-          qutebrowser.enable = false;
-          chromium.enable = true;
-        };
-        chat = {
-          whatsapp.enable = true;
-          zoom.enable = true;
-        };
-        game = {
-          lutris.enable = true;
-          steam.enable = true;
-          wine.enable = true;
-          grapejuice.enable = true;
-        };
-        media = {
-          mpv.enable = true;
-          kdenlive.enable = true;
-        };
+      dwm.enable = true;
+      kitty.enable = true;
+      picom.enable = true;
+      apps = {
+        keepassxc.enable = true;
+        obs.enable = true;
       };
-      dev = {
-        editors = {
-          nvim.enable = true;
-          vscode.enable = true;
-        };
-        cc.enable = true;
-        javascript.enable = true;
-        lua.enable = true;
-        python.enable = true;
-        rust.enable = true;
-        nil.enable = true;
+      browsers = {
+        firefox.enable = true;
+        qutebrowser.enable = false;
+        chromium.enable = true;
       };
-      hardware = {
-        bluetooth.enable = false;
-        nvidia.enable = true;
-        pipewire.enable = true;
+      chat = {
+        whatsapp.enable = true;
+        zoom.enable = true;
+      };
+      game = {
+        lutris.enable = true;
+        steam.enable = true;
+        wine.enable = true;
+        grapejuice.enable = true;
       };
       media = {
-        mpd.enable = true;
-        ncmpcpp.enable = true;
+        mpv.enable = true;
+        kdenlive.enable = true;
       };
-      shell = {
-        nix = {
-          optimization.enable = true;
-          search.enable = true;
-        };
-        zsh.enable = true;
-        tmux.enable = true;
-        ranger.enable = true;
+    };
+    dev = {
+      editors = {
+        nvim.enable = true;
+        vscode.enable = true;
       };
-      work = {
-        libreoffice.enable = true;
+      cc.enable = true;
+      javascript.enable = true;
+      lua.enable = true;
+      python.enable = true;
+      rust.enable = true;
+      nil.enable = true;
+    };
+    hardware = {
+      bluetooth.enable = false;
+      nvidia.enable = true;
+      pipewire.enable = true;
+    };
+    media = {
+      mpd.enable = true;
+      ncmpcpp.enable = true;
+    };
+    shell = {
+      nix = {
+        optimization.enable = true;
+        search.enable = true;
       };
+      zsh.enable = true;
+      tmux.enable = true;
+      ranger.enable = true;
+    };
+    work = {
+      libreoffice.enable = true;
     };
   };
 }
