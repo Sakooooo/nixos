a0489a6a9da18e5f1e2b6d9035d63d32a7125bd0
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-10-04 10:24:23 +0400

    w

--- modules/dev/editors/emacs/default.nix
+++ modules/dev/editors/emacs/default.nix
@@ -16,7 +16,7 @@ in {
   config = mkIf cfg.enable {
     services.emacs = {
       enable = true;
-      package = pkgs.emacs;
+      install = true;
     };
     users.users.sako.packages = with pkgs; [
       # bro
