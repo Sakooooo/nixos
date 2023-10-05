80f7e5c26ed83f852ef61ac5429f0681eb0f1d49
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-10-05 13:55:36 +0400

    finally

--- flake.nix
+++ flake.nix
@@ -18,8 +18,10 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    sops-nix.url = "github:Mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
