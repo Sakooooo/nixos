328a75c8d5e4292044e0944ee19727d42119434f
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-10-10 09:33:20 +0400

    attempt fix two

--- modules/media/mopidy/default.nix
+++ modules/media/mopidy/default.nix
@@ -21,6 +21,15 @@ in {
         [jellyfin]
         hostname = https://jellyfin.sako.box
         username = sako
+        password =
+        libraries = Music
+        albumartistsort = False
+        album_format = {ProductionYear} - {Name}
+
+        [mpd]
+        enabled = true
+        host = 127.0.0.1
+        connection_timeout = 300
       '';
     };
   };
