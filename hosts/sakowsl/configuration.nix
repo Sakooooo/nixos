38d2b683db825a5da7ad06e8e0844549d36443be
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-12-24 22:31:41 +0400

    use windows pinentry for wsl gpg

--- hosts/sakowsl/configuration.nix
+++ hosts/sakowsl/configuration.nix
@@ -64,7 +64,10 @@
   };
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "gnome3";
+    pinentryFlavor = null;
+    settings = {
+      pinentry-program = "/mnt/c/Program Files (x86)/Gpg4win/bin/pinentry.exe";
+    };
   };
   # bare minimum
   environment.systemPackages = with pkgs; [
