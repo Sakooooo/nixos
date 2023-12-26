7a1e9416d4961b9e5b535c8dc2a1b96a596adc29
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2023-12-26 15:02:02 +0400

    add it back again

--- modules/dev/editors/emacs/default.nix
+++ modules/dev/editors/emacs/default.nix
@@ -16,39 +16,39 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # nixpkgs.overlays = [
-    #   inputs.emacs-overlay.overlay
-    # ];
-    # # ues daemon
-    # services.emacs = {
-    #   enable = cfg.daemon;
-    #   install = true;
-    #   #  package = pkgs.emacs29-pgtk;
-    #   package = pkgs.emacsWithPackagesFromUsePackage {
-    #     config = ../../../../config/emacs/emacs.org;
-    #     package = pkgs.emacs-pgtk;
-    #     alwaysEnsure = true;
-    #     alwaysTangle = true;
-    #     extraEmacsPackages = epkgs: [
-    #       epkgs.use-package
-    #     ];
-    #   };
-    # };
-    # users.users.sako.packages = with pkgs; [
-    #   # direnv
-    #   direnv
-    # ];
+    nixpkgs.overlays = [
+      inputs.emacs-overlay.overlay
+    ];
+    # ues daemon
+    services.emacs = {
+      enable = cfg.daemon;
+      install = true;
+      #  package = pkgs.emacs29-pgtk;
+      package = pkgs.emacsWithPackagesFromUsePackage {
+        config = ../../../../config/emacs/emacs.org;
+        package = pkgs.emacs-pgtk;
+        alwaysEnsure = true;
+        alwaysTangle = true;
+        extraEmacsPackages = epkgs: [
+          epkgs.use-package
+        ];
+      };
+    };
+    users.users.sako.packages = with pkgs; [
+      # direnv
+      direnv
+    ];
 
-    # home-manager.users.sako = {lib, ...}: {
-    #   home.file = {
-    #     ".emacs.d/init.el".source = pkgs.runCommand "init.el" {} ''
-    #       cp ${../../../../config/emacs/emacs.org} emacs.org
-    #       ${pkgs.emacs}/bin/emacs -Q --batch ./emacs.org -f org-babel-tangle
-    #       mv init.el $out
-    #     '';
-    #     ".emacs.d/dashboard.png".source = ../../../../config/emacs/dashboard.png;
-    #   };
-    # };
+    home-manager.users.sako = {lib, ...}: {
+      home.file = {
+        ".emacs.d/init.el".source = pkgs.runCommand "init.el" {} ''
+          cp ${../../../../config/emacs/emacs.org} emacs.org
+          ${pkgs.emacs}/bin/emacs -Q --batch ./emacs.org -f org-babel-tangle
+          mv init.el $out
+        '';
+        ".emacs.d/dashboard.png".source = ../../../../config/emacs/dashboard.png;
+      };
+    };
 
     fonts.packages = with pkgs; [
       (nerdfonts.override {fonts = ["JetBrainsMono"];})
