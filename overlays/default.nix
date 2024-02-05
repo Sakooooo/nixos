7fd7464dff96faf6a68378b25dfa7cf8742c961b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-02-05 14:16:46 +0400

    asdfkl;asdfjk;

--- overlays/default.nix
+++ overlays/default.nix
@@ -31,14 +31,12 @@ in {
       src = ../config/dwm;
     });
     nvidiaDriverPackage = prev.nvidiaDriverPackage.overrideAttrs (old: {
-      patches =
-        (old.patches or [])
-        ++ [
-          (prev.fetchpatch {
-            url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-            hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-          })
-        ];
+      patches = [
+        (prev.fetchpatch {
+          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        })
+      ];
     });
   };
 
