40c0bd93fae06c38261a6d4d7fa9cfe275c5b928
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-02-05 14:17:36 +0400

    asdfjk;lasdfjkl;

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,9 +2,7 @@
   inputs,
   config,
   ...
-}: let
-  nvidiaDriverPackage = config.boot.kernelPackages.nvidiaPackages.stable;
-in {
+}: {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../packages {pkgs = final;};
 
@@ -29,14 +27,6 @@ in {
     };
     dwm = prev.dwm.overrideAttrs (old: {
       src = ../config/dwm;
-    });
-    nvidiaDriverPackage = prev.nvidiaDriverPackage.overrideAttrs (old: {
-      patches = [
-        (prev.fetchpatch {
-          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        })
-      ];
     });
   };
 
