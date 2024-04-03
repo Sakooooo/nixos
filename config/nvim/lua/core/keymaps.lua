6fd65fab7ebe35d16f96a18d0d1b1c9efed9f7c1
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-04-03 21:49:47 +0000

    thing

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -51,7 +51,9 @@ vim.keymap.set("n", "nsh", ":split<CR>", opts)
 --vim.keymap.set("n", "<leader>t", ":split<CR> <BAR> :terminal<CR>")
 vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR>")
 vim.keymap.set("n", "<leader>nt", ":FloatermNew --height=25 --width --100<CR>")
-vim.keymap.set("n", "<leader>g", ":FloatermNew --height=29 --width=120 lazygit<CR>")
+
+-- git client
+vim.keymap.set("n", "<leader>gs", ":Neogit")
 
 -- exit terminal with esc
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
