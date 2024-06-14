0b22a66fe36d8908418629025dba2108005671d5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-06-14 18:29:21 +0000

    emacs pinentrny

--- default.nix
+++ default.nix
@@ -30,9 +30,11 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://cache.garnix.io"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
--- hosts/sakowsl/configuration.nix
+++ hosts/sakowsl/configuration.nix
@@ -54,9 +54,11 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://cache.garnix.io"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
@@ -97,7 +99,8 @@
     #pinentryPackage = null;
    settings = {
    #  pinentry-program = "/mnt/c/Program Files (x86)/Gpg4win/bin/pinentry.exe";
-      allow-loopback-entry = true;
+      allow-emacs-pinentry = "";
+      allow-loopback-pinentry = "";
    };
   };
   # bare minimum
