ae96c9dd78e31c3f7433a2333e6bced79face762
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-06-15 11:36:38 +0000

    add pass-sync timer

--- modules/desktop/apps/pass/default.nix
+++ modules/desktop/apps/pass/default.nix
@@ -17,5 +17,28 @@ in {
     users.users.sako.packages = with pkgs; [
       (pass.withExtensions (pkgs: with pkgs; [pass-otp pass-import pass-genphrase pass-checkup]))
     ];
+
+    # systemd timer to run git pull and git push
+    systemd.timers."pass-sync" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "1m";
+        OnUnitActiveSec = "1h";
+        Unit = "pass-sync.service";
+      };
+    };
+
+    systemd.services."pass-sync" = {
+      script = ''
+              set -eu
+              ${pkgs.coreutils}/bin/echo "Hello World"
+              ${pkgs.pass}/bin/pass git pull
+              ${pkgs.pass}/bin/pass git push
+             '';
+      serviceConfig = {
+        Type = "oneshot";
+        User = "sako";
+      };
+    };
   };
 }
