c92c78b98e6819803ec5982c6b5e0f358df42ca2
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-07-04 23:19:01 +0400

    i set the wrong monitor to the primary monitor

--- hosts/sakopc/configuration.nix
+++ hosts/sakopc/configuration.nix
@@ -100,7 +100,7 @@
   # fuck you AOC
   # my monitor only works on 240hz now
   services.xserver.displayManager.setupCommands = ''
-  ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 1920x1080 --rate 239.96 --output HDMI-0 --mode 1920x1080 --primary --left-of DP-0
+  ${pkgs.xorg.xrandr}/bin/xrandr --output DP-0 --mode 1920x1080 --rate 239.96 --primary --output HDMI-0 --mode 1920x1080 --left-of DP-0
   '';
 
   home-manager.users.sako.home.stateVersion = "24.05";
