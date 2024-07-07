813d68780ad9a77d734d9cc03ca4c59dba7444c7
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-07-07 14:12:43 +0400

    (typo) bro

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.enable_tab_bar = false
 
-config.colorscheme = "Tangoesque"
+config.color_scheme = "Tangoesque"
 
 config.window_frame = {
 
