1369bd38a15b852f2ef26ff1b7cef2b8b4a405d3
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-01 16:03:15 +0400

    lets see if this works

--- modules/desktop/apps/localsend/default.nix
+++ modules/desktop/apps/localsend/default.nix
@@ -1,14 +1,13 @@
 { outputs, options, config, lib, pkgs, ...}:
-with lib;
 let
   cfg = config.modules.desktop.apps.localsend;
 in
 {
   options.modules.desktop.apps.localsend = {
-    enable = mkEnableOption false;
+    enable = lib.mkEnableOption false;
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     users.users.sako.packages = with pkgs; [
       localsend
     ];
