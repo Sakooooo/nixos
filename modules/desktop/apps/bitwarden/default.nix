55355f91c5af7eec3ebad56b4ab3da3b7d3afd7d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-01 16:02:10 +0400

    try to get rid of with lib;

--- modules/desktop/apps/bitwarden/default.nix
+++ modules/desktop/apps/bitwarden/default.nix
@@ -6,14 +6,14 @@
   pkgs,
   ...
 }:
-with lib; let
+let
   cfg = config.modules.desktop.apps.bitwarden;
 in {
   options.modules.desktop.apps.bitwarden = {
-    enable = mkEnableOption false;
+    enable = lib.mkEnableOption false;
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     users.users.sako.packages = with pkgs; [
       bitwarden
     ];
