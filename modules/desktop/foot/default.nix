81f4e0909699962bcd15c1af95f92897c170674e
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-03 19:55:05 +0400

    as

---
+++ modules/desktop/foot/default.nix
@@ -0,0 +1,15 @@
+{ options, config, lib, pkgs, ...}:
+let
+  cfg = config.modules.desktop.foot;
+in {
+  options.modules.desktop.foot = {
+    enable = lib.mkEnableOption false;
+  };
+
+  config = lib.mkIf cfg.enable {
+
+  users.users.sako.packages = with pkgs; [
+    foot
+  ];
+  };
+}
