5eb9f2c7a1a234b9dc0e383ddc7cbbf4eb342025
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-05 01:33:48 +0400

    enable gdm again

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -9,15 +9,15 @@ in
 
   config = lib.mkIf cfg.enable {
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
-        user = "greeter";
-      };
-    };
-  };
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
 
   services.xserver = {
       enable = true;
@@ -33,9 +33,9 @@ in
 # #            };
 # #          };
 # #        };
-#         gdm = {
-#           enable = true;
-#         };
+        gdm = {
+          enable = true;
+        };
       # };
       };
       services.libinput = {
