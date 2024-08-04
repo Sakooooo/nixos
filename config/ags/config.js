4cf2505e50034eeb788471e1b305a1764b577ae5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-05 03:27:17 +0400

    maybe this will fix it

--- config/ags/config.js
+++ config/ags/config.js
@@ -123,14 +123,14 @@ function Volume() {
 
 function BatteryLabel() {
     const value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
-    const icon = battery.bind("percent").as(p =>
-        `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
+    // const icon = battery.bind("percent").as(p =>
+        // `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
 
     return Widget.Box({
         class_name: "battery",
         visible: battery.bind("available"),
         children: [
-            Widget.Icon({ icon }),
+            // Widget.Icon({ icon }),
             Widget.LevelBar({
                 widthRequest: 140,
                 vpack: "center",
