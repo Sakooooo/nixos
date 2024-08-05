1d789a6449798df6e11fdb9c7c86887ee80e6647
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-05 05:15:51 +0400

    asuefiwehfo

--- config/ags/config.js
+++ config/ags/config.js
@@ -122,7 +122,7 @@ function Volume() {
 
 
 function BatteryLabel() {
-    const value = battery.percent();
+    const value = battery.bind("percent").as(p => p > 0 ? p / 100 : 0)
     // const icon = battery.bind("percent").as(p =>
         // `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
 
