caa6f1c449854ce32d5d0e6717fc9754ae274f7f
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-05 17:40:10 +0400

    try a new launch script

--- config/ags/launch-ags.sh
+++ config/ags/launch-ags.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-pkill ags
+CONFIG_FILES="/home/sako/.config/ags"
 
-ags &
-    
+trap "killall ags" EXIT
+
+while true; do
+    ags &
+    export APP_PID=$!
+    inotifywait -e create,modify $CONFIG_FILES
+    kill $APP_PID
+done
