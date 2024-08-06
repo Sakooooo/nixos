55ad990bdbb354b5f6b3013e39df3cbf9e523f7d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-06 21:47:33 +0400

    lol

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -123,9 +123,9 @@ in
         # this automatically sets the types
         # thanks PartyWumpus
         # https://github.com/PartyWumpus/dotfiles/blob/277949d84d53a58a3f52be935cd3c581c89d5d7c/modules/hyprland/hyprland.nix#L492
-        # "/nixos/config/ags/types" = {
-        #   source = "${inputs.ags.packages.x86_64-linux.agsWithTypes.out}/share/com.github.Aylur.ags/types";
-        # };
+        "/nixos/config/ags/types" = {
+          source = "${inputs.ags.packages.x86_64-linux.agsWithTypes.out}/share/com.github.Aylur.ags/types";
+        };
       };
       xdg.configFile = {
         hypr = {
