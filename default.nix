08bb26f85dd1ba7b7fd1cf3d632a383a9a4d39e2
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-08-21 17:02:51 +0400

    remove my own cache

--- default.nix
+++ default.nix
@@ -35,14 +35,11 @@
       "https://nix-community.cachix.org"
       # hyprland
       "https://hyprland.cachix.org"
-      # my own cache
-      "https://sako-nixos-cache.cachix.org"
     ];
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "sako-nixos-cache.cachix.org-1:tAbwmFrNrKkIlP/92/R0szKGscNORdS0qNC0iFwgEN8="
     ];
   };
 
