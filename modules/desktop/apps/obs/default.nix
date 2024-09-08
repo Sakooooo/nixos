6bd22c5f48b7e84dc09a9bd1cb99b736f1aaf218
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-08 12:07:59 +0400

    add obs virtual camera support

--- modules/desktop/apps/obs/default.nix
+++ modules/desktop/apps/obs/default.nix
@@ -17,5 +17,15 @@ in {
     users.users.sako.packages = with pkgs; [
       obs-studio
     ];
+
+    # OBS Virtual camera support
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+      v4l2loopback
+    ];
+    boot.kernelModules = [ "v4l2loopback" ];
+    boot.extraModprobeConfig = ''
+       options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    '';
+    security.polkit.enable = true;
   };
 }
