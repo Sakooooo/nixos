5b559b4941e28db2fccf7de166f0c51b17392f85
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-24 10:04:40 +0400

    im slow

--- config/emacs/.offlineimap.py
+++ config/emacs/.offlineimap.py
@@ -40,7 +40,7 @@ def get_client_token(account):
     return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
 
 
-def get_client_token(account):
+def get_client_refresh_token(account):
     cmd = check_output("pass " + account, shell=True)
 
     return re.search(rb"client_refresh_token: (.*)", cmd, flags=0).group(1)
