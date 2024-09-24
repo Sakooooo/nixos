ab63c7a5080407536372e7d95ab2f1f33be554fb
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-24 09:45:15 +0400

    take two

--- config/emacs/.offlineimap.py
+++ config/emacs/.offlineimap.py
@@ -22,146 +22,19 @@ def hydroxide(path):
     return pword
 
 
-class oauth:
-    def get_client_id(account):
-        cmd = check_output("pass " + account, shell=True)
+def get_client_id(account):
+    cmd = check_output("pass " + account, shell=True)
 
-        return re.search(rb"client_id: (.*)", cmd, flags=0).group(1)
+    return re.search(rb"client_id: (.*)", cmd, flags=0).group(1)
 
-    def get_client_secret(account):
-        cmd = check_output("pass " + account, shell=True)
 
-        return re.search(rb"client_secret: (.*)", cmd, flags=0).group(1)
+def get_client_secret(account):
+    cmd = check_output("pass " + account, shell=True)
 
-    def get_client_token(account):
-        cmd = check_output("pass " + account, shell=True)
+    return re.search(rb"client_secret: (.*)", cmd, flags=0).group(1)
 
-        return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
 
+def get_client_token(account):
+    cmd = check_output("pass " + account, shell=True)
 
-#! /usr/bin/env python
-from subprocess import check_output
-import re
-
-
-def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
-
-
-def get_user(account):
-    usercmd = check_output("pass " + account, shell=True)
-
-    return re.search(rb"login: (.*)", usercmd, flags=0).group(1)
-
-
-def hydroxide(path):
-
-    file = open(path, "r")
-
-    pword = file.read()
-
-    return pword
-
-
-class oauth:
-    def get_client_id(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_id: (.*)", cmd, flags=0).group(1)
-
-    def get_client_secret(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_secret: (.*)", cmd, flags=0).group(1)
-
-    def get_client_token(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
-
-
-#! /usr/bin/env python
-from subprocess import check_output
-import re
-
-
-def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
-
-
-def get_user(account):
-    usercmd = check_output("pass " + account, shell=True)
-
-    return re.search(rb"login: (.*)", usercmd, flags=0).group(1)
-
-
-def hydroxide(path):
-
-    file = open(path, "r")
-
-    pword = file.read()
-
-    return pword
-
-
-class oauth:
-    def get_client_id(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_id: (.*)", cmd, flags=0).group(1)
-
-    def get_client_secret(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_secret: (.*)", cmd, flags=0).group(1)
-
-    def get_client_token(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
-
-
-#! /usr/bin/env python
-from subprocess import check_output
-import re
-
-
-def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
-
-
-def get_user(account):
-    usercmd = check_output("pass " + account, shell=True)
-
-    return re.search(rb"login: (.*)", usercmd, flags=0).group(1)
-
-
-def hydroxide(path):
-
-    file = open(path, "r")
-
-    pword = file.read()
-
-    return pword
-
-
-class oauth:
-    def get_client_id(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_id: (.*)", cmd, flags=0).group(1)
-
-    def get_client_secret(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_secret: (.*)", cmd, flags=0).group(1)
-
-    def get_client_token(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
-
-    def get_client_refresh_token(account):
-        cmd = check_output("pass " + account, shell=True)
-
-        return re.search(rb"client_refresh_token: (.*)", cmd, flags=0).group(1)
+    return re.search(rb"client_token: (.*)", cmd, flags=0).group(1)
