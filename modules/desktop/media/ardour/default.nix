acd0b68e00c8095f05a003ccf7af4c64000c6c05
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-09-30 15:56:33 +0400

    media/ardour: add surge-XT

--- modules/desktop/media/ardour/default.nix
+++ modules/desktop/media/ardour/default.nix
@@ -1,15 +1,9 @@
-{ outputs, options, config, lib, pkgs, ...}:
-let
-  cfg = config.modules.desktop.media.ardour;
-in
-{
-  options.modules.desktop.media.ardour = {
-    enable = lib.mkEnableOption false;
-  };
+{ outputs, options, config, lib, pkgs, ... }:
+let cfg = config.modules.desktop.media.ardour;
+in {
+  options.modules.desktop.media.ardour = { enable = lib.mkEnableOption false; };
 
   config = lib.mkIf cfg.enable {
-    users.users.sako.packages = with pkgs; [
-      ardour
-    ];
+    users.users.sako.packages = with pkgs; [ ardour surge-XT ];
   };
 }
