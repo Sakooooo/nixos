61e6471c67da0600ba77f3c337ac4ccdd69b2103
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-02 16:56:02 +0400

    secrets/secrets.nix: uhhh

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   sakotop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGAwG2Fqs3xNF/6/9GdznH/jUIqxW3aTYvmteuq9odZ sako@sakotop";
   sakopc =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjPSt4TykAJgafU9Trk7sr9wzXhBZxawDIZir0CPyDN";
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjPSt4TykAJgafU9Trk7sr9wzXhBZxawDIZir0CPyDN sako@sakopc";
 
   shared = [ sakotop sakopc ];
 
