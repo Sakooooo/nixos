252992006ebcec3fe3d8e2fdd3bcb4424c2fe35e
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-02 13:35:41 +0400

    secrets: init secrets.nix file

---
+++ secrets/secrets.nix
@@ -0,0 +1,9 @@
+let
+  sakotop =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGAwG2Fqs3xNF/6/9GdznH/jUIqxW3aTYvmteuq9odZ sako@sakotop";
+  sakopc =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjPSt4TykAJgafU9Trk7sr9wzXhBZxawDIZir0CPyDN";
+
+  shared = [ sakotop sakopc ];
+
+in { "test.age".publicKeys = shared; }
