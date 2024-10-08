4d1b42e922cf3764cbe69c93f853ec80b76ad9f0
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-08 20:18:16 +0400

    akjlsdf

--- modules/security/tor.nix
+++ modules/security/tor.nix
@@ -4,6 +4,9 @@ in {
   options.modules.security.tor = { enable = lib.mkEnableOption false; };
 
   config = lib.mkIf cfg.enable {
+
+    users.users.sako.packages = with pkgs; [ tor-browser ];
+
     services.tor = {
       enable = true;
       torsocks.enable = true;
