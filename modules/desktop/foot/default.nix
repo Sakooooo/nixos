0b17752bcee74f5e8a7bf0f6d85d573abb0ee473
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-15 18:57:48 +0400

    desktop/foot: home-manager fever dream

--- modules/desktop/foot/default.nix
+++ modules/desktop/foot/default.nix
@@ -1,23 +1,30 @@
-{ options, config, lib, pkgs, ...}:
-let
-  cfg = config.modules.desktop.foot;
+{ options, config, lib, pkgs, ... }:
+let cfg = config.modules.desktop.foot;
 in {
-  options.modules.desktop.foot = {
-    enable = lib.mkEnableOption false;
-  };
+  options.modules.desktop.foot = { enable = lib.mkEnableOption false; };
 
   config = lib.mkIf cfg.enable {
 
-  users.users.sako.packages = with pkgs; [
-    foot
-  ];
+    users.users.sako.packages = with pkgs; [ foot ];
 
-  home-manager.users.sako = {pkgs, ...}: {
-    xdg.configFile = {
-      foot = {
-        source = ../../../config/foot;
+    home-manager.users.sako = { pkgs, ... }: {
+      programs.foot = {
+        enable = true;
+        server.enable = false;
+        settings = {
+          main = {
+            term = "xterm-256color";
+            font = "JetBrainsMono NF:size=12";
+          };
+          scrollback = { lines = 1000; };
+          cursor = {
+            style = "block";
+            blink = "yes";
+            blink-rate = "1000";
+          };
+          mouse = { hide-when-typing = "yes"; };
+        };
       };
     };
-  };
   };
 }
