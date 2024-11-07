dce71aab4e368b2e502dd7bbf8bdeef6ee7ef052
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-11-07 20:45:50 +0400

    nixos moment lmao

--- modules/desktop/tailscale/default.nix
+++ modules/desktop/tailscale/default.nix
@@ -3,5 +3,8 @@ let cfg = config.modules.desktop.tailscale;
 in {
   options.modules.desktop.tailscale = { enable = lib.mkEnableOption false; };
 
-  config = lib.mkIf cfg.enable { services.tailscale = { enable = true; }; };
+  config = lib.mkIf cfg.enable {
+    services.tailscale = { enable = true; };
+    networking.firewall.checkReversePath = "loose";
+  };
 }
