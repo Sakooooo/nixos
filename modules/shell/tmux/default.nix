6adef27fb9728922f0316c9f3e8e4f9073915535
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-11-09 14:01:33 +0400

    add tmux config

--- modules/shell/tmux/default.nix
+++ modules/shell/tmux/default.nix
@@ -1,15 +1,18 @@
-{ outputs, options, config, lib, pkgs, ...}:
-let
-  cfg = config.modules.shell.tmux;
-in
-{
-  options.modules.shell.tmux = {
-    enable = lib.mkEnableOption false;
-  };
+{ outputs, options, config, lib, pkgs, ... }:
+let cfg = config.modules.shell.tmux;
+in {
+  options.modules.shell.tmux = { enable = lib.mkEnableOption false; };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      tmux
-    ];
+    environment.systemPackages = with pkgs; [ tmux ];
+
+    home-manager.users.sako = { pkgs, ... }: {
+      home.file = {
+        ".tmux.conf" = {
+          enable = true;
+          source = ../../../config/tmux.conf;
+        };
+      };
+    };
   };
 }
