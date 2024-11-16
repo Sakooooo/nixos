2c5c16743eae315e4ee284ebe8b7da871a73e25c
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-11-16 23:25:53 +0400

    goodbye audioslider... it was fun...

--- config/ags/widget/Bar.tsx
+++ config/ags/widget/Bar.tsx
@@ -41,19 +41,6 @@ function Wifi() {
     />
 }
 
-function AudioSlider() {
-    const speaker = Wp.get_default()?.audio.defaultSpeaker!
-
-    return <box className="AudioSlider" css="min-width: 140px">
-        <icon icon={bind(speaker, "volumeIcon")} />
-        <slider
-            hexpand
-            onDragged={({ value }) => speaker.volume = value}
-            value={bind(speaker, "volume")}
-        />
-    </box>
-}
-
 function AudioPercent() {
   const speaker = Wp.get_default()?.audio.defaultSpeaker!
 
