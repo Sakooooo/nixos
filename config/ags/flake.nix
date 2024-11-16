2aae98891827d90719ffd8665f16b32695b3dc7f
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-11-16 23:25:34 +0400

    Those who understand the current situation at hand:

--- config/ags/flake.nix
+++ config/ags/flake.nix
@@ -4,25 +4,14 @@
     ags.url = "github:aylur/ags";
   };
 
-  outputs = { self, nixpkgs, ags }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = [
-        pkgs.typescript-language-server
-        # includes astal3 astal4 astal-io by default
-        (ags.packages.${system}.default.overrideAttrs { 
-          extraPackages = [
-                    ags.packages.${pkgs.system}.hyprland
-          ags.packages.${pkgs.system}.mpris
-          ags.packages.${pkgs.system}.battery
-          ags.packages.${pkgs.system}.wireplumber
-          ags.packages.${pkgs.system}.network
-          ags.packages.${pkgs.system}.tray
-          ];
-        })
-      ];
+  outputs = { self, nixpkgs, ags }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs =
+          [ pkgs.typescript-language-server ags.packages.${system}.agsFull ];
+      };
     };
-  };
 }
