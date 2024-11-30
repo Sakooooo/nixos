eba58fc4070ae930a12f83063e207fc1aeacc13d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-11-30 22:45:15 +0400

    Revert "use element-desktop-wayland because total xorg death"
    This reverts commit 95bfeaede40d7bc78be19cc2dd2473bd11172012.

--- modules/desktop/chat/element/default.nix
+++ modules/desktop/chat/element/default.nix
@@ -1,10 +1,22 @@
-{ outputs, options, config, lib, pkgs, ... }:
-let cfg = config.modules.desktop.chat.element;
+{
+  outputs,
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.modules.desktop.chat.element;
 in {
-  options.modules.desktop.chat.element = { enable = lib.mkEnableOption false; };
+  options.modules.desktop.chat.element = {
+    enable = lib.mkEnableOption false;
+  };
 
   config = lib.mkIf cfg.enable {
-    users.users.sako.packages = with pkgs.stable; [ element-desktop-wayland ];
+    users.users.sako.packages = with pkgs.stable; [
+      element-desktop
+    ];
 
   };
 }
