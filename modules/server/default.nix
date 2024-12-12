6b74df4279413561465d7647eae2f10240f940ac
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-12 16:48:14 +0400

    add sakopc ssh keys

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -23,6 +23,7 @@ in {
 
     users.users.sako.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGAwG2Fqs3xNF/6/9GdznH/jUIqxW3aTYvmteuq9odZ sako@sakotop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjPSt4TykAJgafU9Trk7sr9wzXhBZxawDIZir0CPyDN sako@sakopc"
     ];
   };
 }
