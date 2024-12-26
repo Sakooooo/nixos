4791b2187ffdf4937de2716723188c49bf1b2574
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-26 15:26:20 +0400

    add nix langauge server setup to nvim

--- modules/dev/editors/nvim/default.nix
+++ modules/dev/editors/nvim/default.nix
@@ -2,26 +2,6 @@
 let
   sources = import ./sources.nix { inherit pkgs; };
   cfg = config.modules.dev.editors.nvim;
-  configModule = {
-    # custom options 
-    # options = { ... };
-
-    config = {
-      vim = {
-        theme.enable = true;
-        lazy.plugins = {
-          vim-wakatime = {
-            package = sources.vim-wakatime;
-            lazy = false;
-          };
-        };
-      };
-    };
-  };
-  customNeovim = inputs.nvf.lib.neovimConfiguration {
-    inherit pkgs;
-    modules = [ configModule ];
-  };
 in {
   imports = [ inputs.nvf.nixosModules.default ];
   options.modules.dev.editors.nvim = { enable = lib.mkEnableOption false; };
@@ -29,8 +9,6 @@ in {
   config = lib.mkIf cfg.enable {
     # because yes
     # users.users.sako.packages = with pkgs; [
-    #   # neovim
-    #   customNeovim.neovim
     #   # flakes
     #   direnv
     #   # vscode like git
@@ -45,6 +23,13 @@ in {
         # };
         vim = {
           lsp = { enable = true; };
+          languages = {
+             enableLSP = true;
+             nix = {
+                enable = true;
+                format.enable = true;
+             };
+          };
           utility = { vim-wakatime.enable = true; };
         };
       };
