463c732de1d4f2fd297145dd8c34a034aba46b09
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-31 16:37:36 +0400

    sakoserver: init ^w^

--- hosts/sakoserver/configuration.nix
+++ hosts/sakoserver/configuration.nix
@@ -1,11 +1,24 @@
 { config, pkgs, lib, inputs, outputs, ... }: {
   imports = [ outputs.nixosModules.server ./hardware-configuration.nix ];
 
-  # important for later, trust me
+  # its you!
   networking.hostName = "sakoserver";
+
+  # why not
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Dubai";
+
+  # locale
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
 
   boot.loader = {
     timeout = 3;
+    efi.canTouchEfiVariables = true;
     systemd-boot = {
       enable = true;
       configurationLimit = 10;
