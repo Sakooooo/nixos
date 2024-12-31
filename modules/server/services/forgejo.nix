8e30eab2ef766799049b1ec69b5f9080136e0319
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-12-31 19:30:47 +0400

    tupid

--- modules/server/services/forgejo.nix
+++ modules/server/services/forgejo.nix
@@ -2,7 +2,7 @@
 with lib;
 let cfg = config.void.services.forgejo;
 in {
-  options.void.server.forgejo = { enable = mkEnableOption false; };
+  options.void.server.services.forgejo = { enable = mkEnableOption false; };
 
   config = mkIf cfg.enable {
     services.forgejo = {
