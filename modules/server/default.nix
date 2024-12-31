8e715a0a31c2b1163c5c1944ba363e96df0808ea
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-01 03:02:51 +0400

    sakophone ssh key

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -24,6 +24,7 @@ in {
     users.users.sako.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPGAwG2Fqs3xNF/6/9GdznH/jUIqxW3aTYvmteuq9odZ sako@sakotop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjPSt4TykAJgafU9Trk7sr9wzXhBZxawDIZir0CPyDN sako@sakopc"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKprOJ+vtqUL8QQNjRDfIEG7uDPLsxYCpRQoq9blsAvW sakophone"
     ];
 
     users.users.root.openssh.authorizedKeys.keys = [
