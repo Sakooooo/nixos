5051b82f2b8e7c9afc347256b099ba2ba8b7c399
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-01 13:49:32 +0400

    make tailscale exit nodes work

--- modules/server/services/headscale.nix
+++ modules/server/services/headscale.nix
@@ -99,6 +99,11 @@ in {
       webroot = null;
     };
 
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+
     systemd.services = { tailscaled.after = [ "headscale.service" ]; };
 
   };
