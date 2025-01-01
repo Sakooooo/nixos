f576815f7ab3904343da2dcf9ccb96675b3223b5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-01 23:55:46 +0400

    add ddns updater to package/default.nix

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
-{pkgs ? (import ../nixpkgs.nix) {}}: {
+{ pkgs ? (import ../nixpkgs.nix) { } }: {
   # example = pkgs.callPackage ./example { };
-  nullpomino = pkgs.callPackage ./nullpomino.nix {};
+  nullpomino = pkgs.callPackage ./nullpomino.nix { };
+  ddns-updater = pkgs.callPackage ./ddns-updater.nix { };
 }
