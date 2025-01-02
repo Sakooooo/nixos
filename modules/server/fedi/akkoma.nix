623b2c4c3afec5eefa026c112b1365166fe38793
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-02 14:47:50 +0400

    i guess this works

--- modules/server/fedi/akkoma.nix
+++ modules/server/fedi/akkoma.nix
@@ -6,10 +6,59 @@ in {
 
   # :(
   config = mkIf cfg.enable {
+    security.acme.certs = {
+      "social.sako.lol" = { };
+      "media.social.sako.lol" = { };
+    };
     services = {
       akkoma = {
         enable = true;
         package = pkgs.akkoma;
+        frontends = {
+          primary = {
+            package = pkgs.akkoma-frontends.akkoma-fe;
+            name = "akkoma-fe";
+            ref = "stable";
+          };
+          admin = {
+            package = pkgs.akkoma-frontends.admin-fe;
+            name = "admin-fe";
+            ref = "stable";
+          };
+        };
+
+        nginx = {
+          enableACME = true;
+          forceSSL = true;
+          recommendedTlsSettings = true;
+          recommendedOptimisation = true;
+          recommendedGzipSettings = true;
+        };
+
+        config = {
+          ":pleroma" = {
+            ":instance" = {
+              name = "v0id";
+              description = "Good ass fediverse instance";
+              email = "sako@sako.lol";
+              registration_open = false;
+              invites_enabled = true;
+              account_activation_required = false;
+              cleanup_attachments = true;
+              allow_relay = true;
+            };
+            ":media_proxy".enabled = false;
+            "Pleroma.Web.Endpoint" = { url.host = "social.sako.lol"; };
+            "Pleroma.Upload" = {
+              base_url = "https://media.social.sako.lol";
+              filters = map (pkgs.formats.elixirConf { }).lib.mkRaw [
+                "Pleroma.Upload.Filter.Exiftool"
+                "Pleroma.Upload.Filter.Dedupe"
+                "Pleroma.Upload.Filter.AnonymizeFilename"
+              ];
+            };
+          };
+        };
       };
     };
   };
