e9c2657d90adf9ca6fed6f8db34333ae7569cf36
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-03 01:00:26 +0400

    jellyfin

---
+++ modules/server/media/jellyfin.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let cfg = config.void.server.media.jellyfin;
+in {
+  options.void.server.media.jellyfin = { enable = mkEnableOption false; };
+
+  config = mkIf cfg.enable {
+    users.groups.media = { };
+
+    services = {
+      jellyfin = {
+        enable = true;
+        group = "media";
+      };
+
+      nginx.virtualHosts = {
+        "jellyfin.sako.box" = {
+          forceSSL = true;
+          sslCertificate = "/srv/secrets/certs/sako.box.pem";
+          sslCertificateKey = "/srv/secrets/certs/sako.box-key.pem";
+          locations."/" = { proxyPass = "http://localhost:8096"; };
+        };
+      };
+    };
+
+  };
+}
