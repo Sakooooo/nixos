3132d2e91b38c7b8115aeeff06427740bfc2a40b
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-03 23:29:41 +0400

    awegjiopWReg

--- modules/server/services/local/nextcloud/default.nix
+++ modules/server/services/local/nextcloud/default.nix
@@ -35,7 +35,7 @@ in {
           adminpassFile = "/srv/secrets/nextcloud/admin-temp-pass";
         };
         settings = {
-          maintenance_window_start = 1;
+          # maintenance_window_start = 1;
           trusted_domains = [ "https://nextcloud.sako.box" ];
           trusted_proxies = [ "https://nextcloud.sako.box" ];
 
@@ -47,28 +47,28 @@ in {
           default_phone_region = "AE";
           lost_password_link = "disabled";
         };
-        phpOptions = {
-          "opcache.enable" = "1";
-          "opcache.enable_cli" = "1";
-          "opcache.validate_timestamps" = "0";
-          "opcache.save_comments" = "1";
+        #   phpOptions = {
+        #     "opcache.enable" = "1";
+        #     "opcache.enable_cli" = "1";
+        #     "opcache.validate_timestamps" = "0";
+        #     "opcache.save_comments" = "1";
 
-          # <https://docs.nextcloud.com/server/latest/admin_manual/installation/server_tuning.html>
-          "opcache.jit" = "1255";
-          "opcache.jit_buffer_size" = "256M";
+        #     # <https://docs.nextcloud.com/server/latest/admin_manual/installation/server_tuning.html>
+        #     "opcache.jit" = "1255";
+        #     "opcache.jit_buffer_size" = "256M";
 
-          # fix the opcache "buffer is almost full" error in admin overview
-          "opcache.interned_strings_buffer" = "16";
-          # try to resolve delays in displaying content or incomplete page rendering
-          "output_buffering" = "off";
+        #     # fix the opcache "buffer is almost full" error in admin overview
+        #     "opcache.interned_strings_buffer" = "16";
+        #     # try to resolve delays in displaying content or incomplete page rendering
+        #     "output_buffering" = "off";
 
-          "pm" = "dynamic";
-          "pm.max_children" = "50";
-          "pm.start_servers" = "15";
-          "pm.min_spare_servers" = "15";
-          "pm.max_spare_servers" = "25";
-          "pm.max_requests" = "500";
-        };
+        #     "pm" = "dynamic";
+        #     "pm.max_children" = "50";
+        #     "pm.start_servers" = "15";
+        #     "pm.min_spare_servers" = "15";
+        #     "pm.max_spare_servers" = "25";
+        #     "pm.max_requests" = "500";
+        #   };
       };
       nginx.virtualHosts."nextcloud.sako.box" = {
         forceSSL = true;
