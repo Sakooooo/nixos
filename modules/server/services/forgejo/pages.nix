5203b22571399d2ea518aae44823d7072c3abb1a
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-05 16:35:13 +0400

    aerfgjio;wjirgoaewqgiojpihjopqgw

--- modules/server/services/forgejo/pages.nix
+++ modules/server/services/forgejo/pages.nix
@@ -144,6 +144,19 @@ in {
         '';
         locations."/" = { proxyPass = "https://localhost:4563"; };
       };
+      "*.pages.sako.lol" = {
+        # listen = [{
+        #   addr = "0.0.0.0";
+        #   port = 443;
+        #   # ssl = true;
+        # }];
+        forceSSL = true;
+        # enableACME = true;
+        extraConfig = ''
+          proxy_ssl_server_name on;
+        '';
+        locations."/" = { proxyPass = "https://localhost:4563"; };
+      };
     };
     security.acme.certs."pages.sako.lol" = {
       extraDomainNames = [ "*.pages.sako.lol" ];
