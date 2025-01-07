6f0a7cb3d204c61e87e8374aea8900e26f670471
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-07 19:29:10 +0400

    lmao

--- modules/server/game/minecraft/default.nix
+++ modules/server/game/minecraft/default.nix
@@ -8,32 +8,35 @@ in {
 
   config = mkIf cfg.enable {
     # services = {
-    services.minecraft-servers.servers.wires =
-      let modpack = pkgs.fetchPackwizModpack { url = ./sakopack; };
-      in {
-        enable = true;
-        # HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH
-        eula = true;
-        openFirewall = true;
-        package = pkgs.fabricServers.fabric-1_20_4.override {
-          loaderVersion = "0.16.9";
+    services.minecraft-servers = {
+      enable = true;
+      eula = true;
+      servers.wires =
+        let modpack = pkgs.fetchPackwizModpack { url = ./sakopack; };
+        in {
+          enable = true;
+          # HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHHAAHAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH
+          openFirewall = true;
+          package = pkgs.fabricServers.fabric-1_20_4.override {
+            loaderVersion = "0.16.9";
+          };
+          whitelist = { Sakoooo = "6b05caca-3d78-4597-aba5-d0f816f94024"; };
+          serverProperties = {
+            white-list = true;
+            difficulty = "normal";
+            server-port = 25568;
+            gamemode = "survival";
+          };
+          symlinks = { "mods" = "${modpack}/mods"; };
+          # files = {
+          #   "config" = "${modpack}/config";
+          #   "config/mod1.yml" = "${modpack}/config/mod1.yml";
+          #   "config/mod2.conf" = "${modpack}/config/mod2.conf";
+          #   # You can add files not on the modpack, of course
+          #   "config/server-specific.conf".value = { example = "foo-bar"; };
+          # };
+          # };
         };
-        whitelist = { Sakoooo = "6b05caca-3d78-4597-aba5-d0f816f94024"; };
-        serverProperties = {
-          white-list = true;
-          difficulty = "normal";
-          server-port = 25568;
-          gamemode = "survival";
-        };
-        symlinks = { "mods" = "${modpack}/mods"; };
-        # files = {
-        #   "config" = "${modpack}/config";
-        #   "config/mod1.yml" = "${modpack}/config/mod1.yml";
-        #   "config/mod2.conf" = "${modpack}/config/mod2.conf";
-        #   # You can add files not on the modpack, of course
-        #   "config/server-specific.conf".value = { example = "foo-bar"; };
-        # };
-        # };
-      };
+    };
   };
 }
