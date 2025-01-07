1bdebe177152fd6aa84ad1e9c7768e79f2898f1d
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-07 18:51:53 +0400

    add sakopack

---
+++ modules/server/game/minecraft/default.nix
@@ -0,0 +1,31 @@
+{ config, lib, inputs, pkgs, ... }:
+with lib;
+let cfg = config.void.server.game.minecraft;
+in {
+  options.void.server.game.minecraft = { enable = mkEnableOption false; };
+
+  config = mkIf cfg.enable {
+    imports = [ inputs.nix-minecraft.nixosModules.minecraft-server ];
+    nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+    services = {
+      minecraft-servers.servers.wires =
+        let modpack = pkgs.fetchPackwizModpack { url = ./sakopack; };
+        in {
+          enable = true;
+          package = pkgs.fabrticServers.fabric-1_24_4.override {
+            loaderVersion = "0.16.9";
+          };
+          whielist = { Sakoooo = "6b05caca-3d78-4597-aba5-d0f816f94024"; };
+          symlinks = { "mods" = "${modpack}/mods"; };
+          # files = {
+          #   "config" = "${modpack}/config";
+          #   "config/mod1.yml" = "${modpack}/config/mod1.yml";
+          #   "config/mod2.conf" = "${modpack}/config/mod2.conf";
+          #   # You can add files not on the modpack, of course
+          #   "config/server-specific.conf".value = { example = "foo-bar"; };
+          # };
+        };
+    };
+  };
+}
