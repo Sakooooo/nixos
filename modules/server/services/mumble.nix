5d96515af3006ae8d362fc331f691dde827ba5e2
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-12 14:25:21 +0400

    asdf

--- modules/server/services/mumble.nix
+++ modules/server/services/mumble.nix
@@ -9,18 +9,17 @@ in {
       murmur = {
         enable = true;
         port = 64738;
-        # Fuck you
-        group = "nginx";
         openFirewall = true;
         welcometext = "sako.lol!";
         sslKey = "/var/lib/acme/mumble.sako.lol/key.pem";
-        sslCert = "/var/lib/acme/mumbe.sako.lol/fullchain.pem";
+        sslCert = "/var/lib/acme/mumble.sako.lol/fullchain.pem";
         bandwidth = 72000;
         clientCertRequired = true;
       };
     };
 
     security.acme.certs."mumble.sako.lol" = {
+      group = config.services.murmur.group;
       credentialsFile = "/srv/secrets/porkbun";
       dnsProvider = "porkbun";
       webroot = null;
