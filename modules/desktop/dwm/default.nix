b5e50ceeea457cb91f941dceb82339411d684ccf
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-25 23:13:49 +0400

    do some more stuff to the dwm module

--- modules/desktop/dwm/default.nix
+++ modules/desktop/dwm/default.nix
@@ -16,14 +16,25 @@ in {
     xdg.portal.enable = true;
     xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
-#   services.displayManager.defaultSession = "dwm";
+    #   services.displayManager.defaultSession = "dwm";
+
+    services.libinput = {
+      enable = true;
+
+      # no mouse accel
+      mouse = {accelProfile = "flat";};
+
+      # no touchpad accel
+      touchpad = {accelProfile = "flat";};
+    };
+
     services.xserver = {
       enable = true;
       windowManager.dwm.enable = true;
       windowManager.dwm.package = pkgs.dwm.overrideAttrs {
         src = ../../../config/dwm;
       };
-      displayManager.lightdm = {
+      displayManapger.lightdm = {
         enable = true;
         greeters.gtk = {enable = true;};
       };
@@ -35,5 +46,28 @@ in {
       brightnessctl
       flameshot
     ];
+
+    home-manager.users.sako = {pkgs, ...}: {
+      home.pointerCursor = {
+        # name = "Catppuccin-Mocha-Dark";
+        name = "BreezeX-RosePine-Linux";
+        size = 24;
+        gtk.enable = true;
+        package = pkgs.rose-pine-cursor;
+      };
+      gtk = {
+        enable = true;
+        theme.name = "Fluent-pink-Dark";
+        theme.package = pkgs.fluent-gtk-theme;
+        iconTheme.name = "Fluent-pink-dark";
+        iconTheme.package = pkgs.fluent-icon-theme;
+      };
+      home.file = {
+        "background.png" = {
+          enable = true;
+          source = ../../../config/background.png;
+        };
+      };
+    };
   };
 }
