9997a99989d064ab9ec8b1261675954962c69012
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-01-25 18:46:31 +0400

    bleh

--- modules/desktop/xfce/default.nix
+++ modules/desktop/xfce/default.nix
@@ -1,26 +1,37 @@
-{ inputs, outputs, options, config, lib, pkgs, ... }:
-let cfg = config.modules.desktop.xfce;
+{
+  inputs,
+  outputs,
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.modules.desktop.xfce;
 in {
-  options.modules.desktop.xfce = { enable = lib.mkEnableOption false; };
+  options.modules.desktop.xfce = {enable = lib.mkEnableOption false;};
 
   config = lib.mkIf cfg.enable {
     modules.desktop.dunst.enable = lib.mkForce false;
 
     # for flatpak
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+
+    # things
+    services.gnome.gnome-keyring.enable = true;
 
     services.displayManager.defaultSession = "xfce";
     services.xserver = {
       enable = true;
-      desktopManager = { xfce.enable = true; };
+      desktopManager = {xfce.enable = true;};
       displayManager.lightdm = {
         enable = true;
-        greeters.gtk = { enable = true; };
+        greeters.gtk = {enable = true;};
       };
     };
 
-    home-manager.users.sako = { pkgs, ... }: {
+    home-manager.users.sako = {pkgs, ...}: {
       home.file = {
         "background.png" = {
           enable = true;
