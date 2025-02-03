335fc1b48ec6d185bcebe275b93933883b112a71
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-03 21:51:35 +0400

    asdkljfasdlkjasdfjkl;asdfjkl

--- modules/server/services/attic.nix
+++ modules/server/services/attic.nix
@@ -28,7 +28,7 @@ in {
             path = "/var/lib/atticd/storage";
           };
 
-          database.url = "postgres://attic:attic@localhost:5432/attic";
+          database.url = "postgres://attic@localhost/attic";
 
           # Data chunking
           #
