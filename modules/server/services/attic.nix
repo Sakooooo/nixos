5aaeab3e2192273488885c5a9c7cc7488c729b85
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-03 20:56:28 +0400

    didnt work

--- modules/server/services/attic.nix
+++ modules/server/services/attic.nix
@@ -12,48 +12,46 @@ in {
 
   config = mkIf cfg.enable {
     # copied from the docs LOL
-    services = {
-      atticd = {
-        enable = true;
+    inputs.attic.nixosModules.atticd.atticd = {
+      enable = true;
 
-        # Replace with absolute path to your environment file
-        environmentFile = "/etc/atticd.env";
+      # Replace with absolute path to your environment file
+      environmentFile = "/etc/atticd.env";
 
-        settings = {
-          listen = "[::]:34753";
+      settings = {
+        listen = "[::]:34753";
 
-          jwt = {};
+        jwt = {};
 
-          # Data chunking
+        # Data chunking
+        #
+        # Warning: If you change any of the values here, it will be
+        # difficult to reuse existing chunks for newly-uploaded NARs
+        # since the cutpoints will be different. As a result, the
+        # deduplication ratio will suffer for a while after the change.
+        chunking = {
+          # The minimum NAR size to trigger chunking
           #
-          # Warning: If you change any of the values here, it will be
-          # difficult to reuse existing chunks for newly-uploaded NARs
-          # since the cutpoints will be different. As a result, the
-          # deduplication ratio will suffer for a while after the change.
-          chunking = {
-            # The minimum NAR size to trigger chunking
-            #
-            # If 0, chunking is disabled entirely for newly-uploaded NARs.
-            # If 1, all NARs are chunked.
-            nar-size-threshold = 64 * 1024; # 64 KiB
+          # If 0, chunking is disabled entirely for newly-uploaded NARs.
+          # If 1, all NARs are chunked.
+          nar-size-threshold = 64 * 1024; # 64 KiB
 
-            # The preferred minimum size of a chunk, in bytes
-            min-size = 16 * 1024; # 16 KiB
+          # The preferred minimum size of a chunk, in bytes
+          min-size = 16 * 1024; # 16 KiB
 
-            # The preferred average size of a chunk, in bytes
-            avg-size = 64 * 1024; # 64 KiB
+          # The preferred average size of a chunk, in bytes
+          avg-size = 64 * 1024; # 64 KiB
 
-            # The preferred maximum size of a chunk, in bytes
-            max-size = 256 * 1024; # 256 KiB
-          };
+          # The preferred maximum size of a chunk, in bytes
+          max-size = 256 * 1024; # 256 KiB
         };
       };
-      nginx.virtualHosts."cache.sako.lol" = {
-        forceSSL = true;
-        enableACME = true;
-        http3 = true;
-        locations."/" = {proxyPass = "http://localhost:34753";};
-      };
+    };
+    services.nginx.virtualHosts."cache.sako.lol" = {
+      forceSSL = true;
+      enableACME = true;
+      http3 = true;
+      locations."/" = {proxyPass = "http://localhost:34753";};
     };
 
     security.acme.certs."cache.sako.lol" = {
