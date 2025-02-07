a4dcf08e02f735d8f55ef7d5e9c666fa7b193504
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-07 21:47:03 +0400

    nixpkgs jellyfin-web intro-skipper

--- modules/server/media/jellyfin.nix
+++ modules/server/media/jellyfin.nix
@@ -12,6 +12,28 @@ in {
   config = mkIf cfg.enable {
     users.groups.media = {};
 
+    # intro-skipper plugin, https://github.com/intro-skipper/intro-skipper
+    # taken from https://wiki.nixos.org/wiki/Jellyfin#Intro_Skipper_plugin
+    nixpkgs.overlays = with pkgs; [
+      (
+        final: prev: {
+          jellyfin-web = prev.jellyfin-web.overrideAttrs (finalAttrs: previousAttrs: {
+            installPhase = ''
+              runHook preInstall
+
+              # this is the important line
+              sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+              mkdir -p $out/share
+              cp -a dist $out/share/jellyfin-web
+
+              runHook postInstall
+            '';
+          });
+        }
+      )
+    ];
+
     services = {
       jellyfin = {
         enable = true;
