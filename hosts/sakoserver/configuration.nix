3af883f26c1f0722c15244b87d4569b931213bd0
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-14 19:50:58 +0400

    optimize (hopefully) networking (thanks notashelf/nyx on github)

--- hosts/sakoserver/configuration.nix
+++ hosts/sakoserver/configuration.nix
@@ -39,9 +39,61 @@
     };
   };
 
-  # Is that
-  # HEEERO ON A PLASTIC HORSE
+  # TODO Move this to a module
+  boot.kernelModules = ["tls" "tcp_bbr"];
   boot.kernel.sysctl = {
+    # From https://github.com/NotAShelf/nyx/blob/d407b4d6e5ab7f60350af61a3d73a62a5e9ac660/modules/core/common/system/os/networking/optimize.nix
+    # TCP hardening
+    # Prevent bogus ICMP errors from filling up logs.
+    "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+    # Reverse path filtering causes the kernel to do source validation of
+    # packets received from all interfaces. This can mitigate IP spoofing.
+    "net.ipv4.conf.default.rp_filter" = 1;
+    "net.ipv4.conf.all.rp_filter" = 1;
+    # Do not accept IP source route packets (we're not a router)
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+    # Don't send ICMP redirects (again, we're on a router)
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.send_redirects" = 0;
+    # Refuse ICMP redirects (MITM mitigations)
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+    # Protects against SYN flood attacks
+    "net.ipv4.tcp_syncookies" = 1;
+    # Incomplete protection again TIME-WAIT assassination
+    "net.ipv4.tcp_rfc1337" = 1;
+    # And other stuff
+    "net.ipv4.conf.all.log_martians" = true;
+    "net.ipv4.conf.default.log_martians" = true;
+    "net.ipv4.icmp_echo_ignore_broadcasts" = true;
+    "net.ipv6.conf.default.accept_ra" = 0;
+    "net.ipv6.conf.all.accept_ra" = 0;
+    "net.ipv4.tcp_timestamps" = 0;
+
+    # TCP optimization
+    # TCP Fast Open is a TCP extension that reduces network latency by packing
+    # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
+    # both incoming and outgoing connections:
+    "net.ipv4.tcp_fastopen" = 3;
+    # Bufferbloat mitigations + slight improvement in throughput & latency
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = "cake";
+
+    # This bit is taken from the link above
+    "net.ipv4.tcp_tw_reuse" = 1;
+    "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "net.ipv4.tcp_mtu_probing" = 1;
+    # These look problematic, note to self remove if i broke it lol
+    "net.netfilter.nf_conntrack_generic_timeout" = 60;
+    "net.netfilter.nf_conntrack_max" = 1048576;
+    "net.netfilter.nf_conntrack_tcp_timeout_established" = 600;
+    "net.netfilter.nf_conntrack_tcp_timeout_time_wait" = 1;
+
     # https://wiki.archlinux.org/title/Sysctl#Improving_performance
     # increase recieve queue size
     "net.core.netdev_max_backlog" = 16384;
