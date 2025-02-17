ae8bf61b7397308417177b48f6f4d74434fdca77
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-02-17 23:00:28 +0400

    add hardware for intel gpu on sakoserver

--- hosts/sakoserver/configuration.nix
+++ hosts/sakoserver/configuration.nix
@@ -129,6 +129,21 @@
     programs.bash.enable = true;
   };
 
+  # actual idiot
+  # need for hardware transcoding
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     neovim
