10da2b8e0135be2c13ba1e08dd265956331df53a
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2025-03-31 18:42:39 +0400

    kdenlive -> kdePackages.kdenlive

--- modules/desktop/media/kdenlive/default.nix
+++ modules/desktop/media/kdenlive/default.nix
@@ -1,15 +1,20 @@
-{ outputs, options, config, lib, pkgs, ...}:
-let
-  cfg = config.modules.desktop.media.kdenlive;
-in
 {
+  outputs,
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.modules.desktop.media.kdenlive;
+in {
   options.modules.desktop.media.kdenlive = {
     enable = lib.mkEnableOption false;
   };
 
   config = lib.mkIf cfg.enable {
     users.users.sako.packages = with pkgs; [
-      kdenlive 
+      kdePackages.kdenlive
       mediainfo
       glaxnimate
     ];
